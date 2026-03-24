80ff00437c1ede2c5518cff3a96acbaa34816a2c
tgithinji <ted@teddygithinji.me>
2026-03-24 16:21:13 +0300

    Create User model and configure alembic migrations

---
+++ src/auth/models.py
@@ -0,0 +1,42 @@
+from sqlmodel import SQLModel, Field
+from sqlalchemy import Column
+import sqlalchemy.dialects.postgresql as pg
+from datetime import datetime, date, timezone
+import uuid
+
+
+class User(SQLModel, table=True):
+    __tablename__ = "users"
+
+    id: uuid.UUID = Field(
+        sa_column=Column(
+            pg.UUID,
+            primary_key=True,
+            nullable=False,
+            default=uuid.uuid4
+        )
+    )
+    username: str = Field(max_length=255, index=True)
+    email: str = Field(max_length=255)
+    first_name: str = Field(max_length=255)
+    last_name: str = Field(max_length=255)
+    is_verified: bool = Field(default=False)
+    created_at: datetime = Field(
+        sa_column=Column(
+            pg.TIMESTAMP,
+            nullable=False,
+            default=lambda: datetime.now()
+        )
+    )
+
+    updated_at: datetime = Field(
+        sa_column=Column(
+            pg.TIMESTAMP,
+            nullable=False,
+            default=lambda: datetime.now(),
+            onupdate=lambda: datetime.now()
+        )
+    )
+
+    def __repr__(self):
+        return f"<User = {self.username}>"
